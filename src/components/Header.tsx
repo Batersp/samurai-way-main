cd4349e5eaf505e454e247d4a2ae4087ae611c26
pavel_ishmukov <p-pup1@mail.ru>
2022-04-02 10:58:42 +0300

    12 lesson is over

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://upload.wikimedia.org/wikipedia/ru/thumb/5/53/Arsenal_FC.svg/1200px-Arsenal_FC.svg.png'/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://www.arsenal.com/sites/default/files/styles/large_16x9/public/images/MOTM.jpg?itok=JOg4NtNr'/>
+            </div>
+            <div>
+                ava+discription
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                    <div>
+                        post 3
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
