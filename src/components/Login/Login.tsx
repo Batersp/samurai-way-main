8e6c53aaa0c049a9c8dc50665db8dbd62ae15c8b
pavel_ishmukov <p-pup1@mail.ru>
2022-07-04 22:51:53 +0300

    added validation to the Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,29 +4,65 @@ import {loginFields} from "./loginInterface";
 
 export const Login = () => {
 
-    const {register, handleSubmit, formState: {errors}} = useForm<loginFields>()
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<loginFields>({mode: "onBlur"})
 
-    const onSubmit: SubmitHandler<loginFields> = (data) => console.log(data)
+    const onSubmit: SubmitHandler<loginFields> = (data) => {
+        console.log(data)
+        reset()
+    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
-                <input {...register("login", {required: 'Name is required field'})}
-                       type="text"
-                       placeholder={'enter login'}
-                />
-                <span>Login</span>
+
+                <label>
+                    Login
+                    <div>
+                        <input {...register("login", {
+                            required: 'Name is required field',
+                            minLength: {
+                                value: 6,
+                                message: 'minimum 6 characters'
+                            }
+                        })}
+                               type="text"
+                               placeholder={'enter login'}
+                        />
+                    </div>
+                </label>
+
                 {errors.login && <div style={{color: 'red', fontWeight: 'bold'}}>{errors.login.message}</div>}
             </div>
             <div>
-                <input {...register("password", {required: 'Password is required field'})}
-                       type="text"
-                       placeholder={'enter password'}
-                />
-                <span>Password</span>
+                <label>
+                    Password
+                    <div>
+                        <input {...register("password", {
+                            required: 'Password is required field',
+                            minLength: {
+                                value: 6,
+                                message: 'minimum 6 characters'
+                            },
+                            maxLength: {
+                                value: 20,
+                                message: 'maximum 20 characters'
+                            },
+                            pattern: {
+                                value: /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/,
+                                message: 'password must contain at least one number, one special character, uppercase and lowercase letters'
+                            }
+                        })}
+                               type="text"
+                               placeholder={'enter password'}
+                        />
+                    </div>
+                </label>
                 {errors.password && <div style={{color: 'red', fontWeight: 'bold'}}>{errors.password.message}</div>}
             </div>
-            <div><input {...register("rememberMe")} type="checkbox"/></div>
+            <label>
+                <input {...register("rememberMe")} type="checkbox"/>
+                Remember me
+            </label>
             <div>
                 <button>Send</button>
             </div>
